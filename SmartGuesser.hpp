2a1b98f192264f822e65b0e08410e407f721aa3d
MoranOshia <44996337+MoranOshia@users.noreply.github.com>
2019-05-08 03:19:50 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,21 +1,29 @@
-/**
- * Header file for the SmartGuesser class. 
- * @author Israel Buskila and Moran Oshia
- * @since 04-2019
- */
 #pragma once
 #include "Guesser.hpp"
-using std::string;
+#include "calculate.hpp"
+#include <list>
 
-class SmartGuesser: public bullpgia::Guesser {
-		string mySmartString;
-	public:
-        uint length;
-        SmartGuesser(){}
-		SmartGuesser(const string& smartString) { mySmartString = smartString; }
-		std::string guess() override ;
-	  
-        void  startNewGame(uint length)override;
-    
-    std::string learn(std::string reply)override;
+namespace bullpgia
+{
+
+class SmartGuesser : public bullpgia::Guesser
+{
+
+private:
+
+  list<std::string> combination;
+  std::string temp;
+
+public:
+
+  string guess() override;
+  void learn(string result)override;
+  void startNewGame(uint length)override;
+  void newList();
+  void removeComb(string result);
+
 };
+
+
+
+} 
